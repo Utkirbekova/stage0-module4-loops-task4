70ebb1f783a692f5946a0ffbb6bfb9234a060446
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-26 10:35:29 +0300

    .

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,7 +3,10 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
         System.out.println("iterating till the end");
-        if (toBreakWith == 5 && numberToGoUntil == 10) System.out.print("1\n2\n3\n4\n5\n");
-        if (toBreakWith == 6 && numberToGoUntil == 5) System.out.print("iterating till the end\n1\n2\n3\n4\n5\n");
+        for (int i = 1; i <= numberToGoUntil; i++) {
+            System.out.println(i);
+
+
+        }
     }
 }
